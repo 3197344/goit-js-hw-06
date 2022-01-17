60c116fffe9ed31354bdbc7e22554595c7ca1962
Nadia Lut <3197344@gmail.com>
2022-01-17 16:34:26 +0200

    правки после Артема

--- js/task-04.js
+++ js/task-04.js
@@ -3,24 +3,44 @@
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или
 //  уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
-
-let counterValue = document.querySelector('#value');
-let valueEl = Number(document.querySelector('#value').textContent);
+let counterValue = 0;
+let valueEl = document.querySelector('#value');
 
 const minusBtn = document.querySelector('#counter').firstElementChild;
 const addBtn = document.querySelector('#counter').lastElementChild;
 
-addBtn.addEventListener('click', element => {
-console.log("Вешаю слушателя события на целевую кнопку");
-valueEl += 1;
-counterValue.innerText = valueEl;
-console.log("значение", counterValue);
-});
+minusBtn.addEventListener('click', addValue);
+addBtn.addEventListener('click', minusValue);
 
-minusBtn.addEventListener('click', element => {
-console.log("Снимаю слушателя события с целевой кнопки");
-valueEl -= 1;
+function minusValue() {
+  counterValue += 1;
+    valueEl.innerHTML = counterValue;
+    console.log("Вешаю слушателя события с целевой кнопки");
+}
 
-counterValue.innerText = valueEl;
-console.log("значение", counterValue);
-});
+function addValue() {
+  counterValue -= 1;
+    valueEl.innerHTML = counterValue;
+    console.log("Снимаю слушателя события на целевую кнопку");
+} 
+
+// let counterValue = document.querySelector('#value');
+// let valueEl = Number(document.querySelector('#value').textContent);
+
+// const minusBtn = document.querySelector('#counter').firstElementChild;
+// const addBtn = document.querySelector('#counter').lastElementChild;
+
+// addBtn.addEventListener('click', element => {
+// console.log("Вешаю слушателя события на целевую кнопку");
+// valueEl += 1;
+// counterValue.innerText = valueEl;
+// console.log("значение", counterValue);
+// });
+
+// minusBtn.addEventListener('click', element => {
+// console.log("Снимаю слушателя события с целевой кнопки");
+// valueEl -= 1;
+
+// counterValue.innerText = valueEl;
+// console.log("значение", counterValue);
+// });
